9961c1ca2067b150142fb08c8cdd8555c80756c6
Max <arcas.llc@yandex.ru>
2017-09-26 15:55:48 +0300

    Admin panel, medicals page

--- src/main/java/main/dao/ReagentDao.java
+++ src/main/java/main/dao/ReagentDao.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface ReagentDao extends AbstractDao {
 
     public Reagent get (long id );
-    public List<Reagent> getAll();
-    public List<Reagent> getPage(int firstResult);
+    public List<Reagent> getAll(String kind);
+    public List<Reagent> getPage(int firstResult,  String kind);
     public List<Reagent> searchByName(String value, String type);
-    public Long getCount();
+    public Long getCount(String kind);
 }
